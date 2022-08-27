24bdf20235d0dd9ffab4299b0178d5c72d805eff
ppsingh <tech.ppsingh@gmail.com>
2022-08-27 20:35:32 +0530

    firt init commit

---
+++ src/test/java/com/nit/pawan/SpringBoot2WebMvcRequestParamEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit.pawan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2WebMvcRequestParamEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
