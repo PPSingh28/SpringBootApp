5e21d54213a2bc71a00076c11c6367ff6d427b8e
ppsingh <tech.ppsingh@gmail.com>
2022-08-27 21:23:04 +0530

    run method modified

--- src/main/java/com/nit/pawan/SpringBoot2WebMvcRequestParamEx1Application.java
+++ src/main/java/com/nit/pawan/SpringBoot2WebMvcRequestParamEx1Application.java
@@ -8,6 +8,7 @@ public class SpringBoot2WebMvcRequestParamEx1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot2WebMvcRequestParamEx1Application.class, args);
+		System.out.println("method modified");
 	}
 
 }
